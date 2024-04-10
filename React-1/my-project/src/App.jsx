f23209813e001d1dc6cfb7c6e2d3445543042b9a
sagarchaurasia176 <sagarchaurasia176@gmail.com>
2024-04-10 15:36:13 +0530

    gifProject

---
+++ React-1/my-project/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import '@radix-ui/themes/styles.css';
+import ContainerPage from './Components/ContainerPage'
+function App() {
+
+  return (
+    <>
+      <div className=' bg-slate-950  h-screen w-auto'>
+          <ContainerPage />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ React-1/my-project/src/Components/ContainerPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Box from "./Box";
+
+function ContainerPage() {
+  return (
+    <div className=" bg-slate-950 h-auto w-auto">
+      <Header />
+      <Box />
+
+    </div>
+  );
+}
+
+export default ContainerPage;
