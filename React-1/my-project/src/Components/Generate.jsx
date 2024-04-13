ec316142e7926b8bf8411ada64bf62e49731b49b
sagarchaurasia176 <sagarchaurasia176@gmail.com>
2024-04-13 11:38:10 +0530

    code updated

--- React-1/my-project/src/Components/Generate.jsx
+++ React-1/my-project/src/Components/Generate.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 
-function Generate({RandomGenerate}) {
+function Generate({ RandomGenerate }) {
   // const { RandomGenerate } = RandomGenerate.props;
   // const { setLoading } = setLoading.props;
 
   return (
     <div>
       <button
-        
         onClick={RandomGenerate}
         class="bg-violet-500 p-1 rounded-md
     hover:bg-violet-600 active:bg-violet-700 
     focus:outline-none focus:ring focus:ring-violet-300 ..."
-    >
+      >
         Click TO Generate
       </button>
     </div>
